0b84865cc27ca74af996b324231d178c2f396eb7
Aaronnc7 <anova@usm.cl>
2024-11-15 00:15:30 -0300

    creacion de preguntas

---
+++ intro/quiz/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from .import views
+
+urlpatterns = [
+   path('', views.home , name = 'home'),
+   path('quiz/', views.quiz, name= 'quiz'),
+   path('api/get-quiz/', views.get_quiz, name='get_quiz')
+ ]
